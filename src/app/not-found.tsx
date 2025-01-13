1dbdf1e4ee2ed4096b0cfc8d10a53e5e357093eb
Alexei <150178624+Cyber-Alexei@users.noreply.github.com>
2025-01-13 09:51:49 -0600

    image component used for the image in the 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,25 @@
 "use client";
+import Image from "next/image";
 
 export default function NotFound() {
   return (
     <div className="h-[100vh] w-[100vw]">
-      <div className="relative flex flex-col h-full w-full items-center justify-center bg-[url(/images/404image.png)] bg-contain bg-no-repeat bg-center">
+      <div className="relative flex flex-col h-full w-full items-center justify-center">
+        <Image
+          alt="tree for 404 page"
+          src="/images/404image.png"
+          fill={true}
+          priority={true}
+          quality={100}
+          style={{
+            position: "absolute",
+            objectFit: "contain",
+            objectPosition: "center",
+          }}
+        />
         <p
           style={{ textShadow: "0px 0px 10px black" }}
-          className="text-[100px] sm:text-[150px] md:text-[200px] lg:text-[300px] text-white font-extrabold"
+          className="absolute text-[100px] sm:text-[150px] md:text-[200px] lg:text-[300px] text-white font-extrabold"
         >
           404
         </p>
