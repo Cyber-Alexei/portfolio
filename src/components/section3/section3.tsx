e3fb434739008c60ea2856c457ca99e31df02ba2
Alexei <150178624+Cyber-Alexei@users.noreply.github.com>
2025-01-12 22:43:09 -0600

    all images for movil devices added in the home page and 404 page

--- src/components/section3/section3.tsx
+++ src/components/section3/section3.tsx
@@ -39,7 +39,7 @@ export default function Section3() {
         {/*MAIN DIV*/}
         <div className="w-full h-full flex items-center">
           {/*IMAGE*/}
-          <div className="sm:w-[600px] sm:h-[600px] md:w-[80%] md:h-[80%] absolute right-[-50px] bottom-0 bg-[url(/images/branch2.png)] w-[550px] h-[550px] bg-contain bg-no-repeat bg-right"></div>
+          <div className="sm:w-[600px] sm:h-[600px] md:w-[80%] md:h-[80%] absolute right-[-50px] bottom-0 bg-[url(/images/branch2-movil.png)] md:bg-[url(/images/branch2.png)] w-[550px] h-[550px] bg-contain bg-no-repeat bg-right"></div>
           {/*CONTENT DIV*/}
           <div className="absolute flex items-center justify-center w-full h-full z-10">
             <div className="relative sm:w-[80%] md:w-[70%] lg:w-[770px] lg:h-[560px] w-[90%] h-[60%] bg-[rgba(240,240,240,0.5)] flex flex-col gap-6 items-center justify-center border border-solid border-[#8b9339]">
